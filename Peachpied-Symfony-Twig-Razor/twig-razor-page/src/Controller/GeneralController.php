e831f0026e9ad71b551fd0db4973c394e1312d3c
Filip Horky <Fezzzi@users.noreply.github.com>
2019-08-05 13:19:31 +0200

    Update GeneralController.php

--- Peachpied-Symfony-Twig-Razor/twig-razor-page/src/Controller/GeneralController.php
+++ Peachpied-Symfony-Twig-Razor/twig-razor-page/src/Controller/GeneralController.php
@@ -11,51 +11,26 @@ class GeneralController
 {
     public function indexAction()
     {
-	$loader = new FilesystemLoader("templates/");
-        //$twig = new Environment($loader);
-
-        //$str = $twig->render('base.html.twig', ['the' => 'variables', 'go' => 'here']);
-        //return new Response($str);
-
-		$file = '/index.html';
+		$file = 'index.html';
 		$response = new BinaryFileResponse($file);
-		return $repsonse;
-
-        return new Response(
-            '<html>
-                <head>
-                    <link rel="stylesheet" href="GeneralStyles.css" type="text/css">
-                    
-                </head>
-                <body>
-                    <h1>RAZOR and TWIG interoperability</h1>
-                    <h3>minimal exmaple</h3>
-                    <div class="tile-row">
-                        <a href="/razor-page" class="tile t1"><div><h2>Razor Page</h2></div></a>
-                        <a href="/twig-page" class="tile t2"><div><h2>Twig Page</h2></div></a>
-                    </div>
-                    <div class="tile-row">
-                        <a href="/twig-in-razor-page" class="tile t3"><div><h2>Razor Page</h2><h5>with embedded Twig</h5></div></a>
-                        <a href="/razor-in-twig-page" class="tile t4"><div><h2>Twig Page</h2><h5>with embedded Razor</h5></div></a>
-                    </div>
-                </body>
-            </html>'
-        );
+		return $response;
     }
 	
 	public function twigPageAction()
 	{
-        $loader = new FilesystemLoader("./templates/");
+        $loader = new FilesystemLoader("../templates/");
         $twig = new Environment($loader);
 
-        $str = $twig->render('base.html.twig', ['the' => 'variables', 'go' => 'here']);
+        $str = $twig->render('twig-page.html.twig', ['the' => 'variables', 'go' => 'here']);
         return new Response($str);
 	}
 	
 	public function razorInTwigPageAction()
 	{
-		return new Response(
-            '<html><body><h1>razorInTwigPage</h1></body></html>'
-        );
+		$loader = new FilesystemLoader("../templates/");
+        $twig = new Environment($loader);
+
+        $str = $twig->render('razor-in-twig-page.html.twig', ['the' => 'variables', 'go' => 'here']);
+        return new Response($str);
 	}
 }
