95cb7df932278a2aaaf8e4613aaa58488a7408d4
Filip Horky <Fezzzi@users.noreply.github.com>
2019-09-18 03:26:48 +0200

    minimal edits

--- Peachpied-Symfony-Twig-Razor/Server/Program.cs
+++ Peachpied-Symfony-Twig-Razor/Server/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 
 namespace Peachpied.Symfony.Twig.Razor.Server
 {
